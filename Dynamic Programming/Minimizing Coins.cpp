254aeba1a4bcaee602de2c7cde202dd3b9dfaf6d
Mohamed Harby <71990569+Mohamed-Harby@users.noreply.github.com>
2021-09-11 23:47:47 +0200

    Create Minimizing Coins.cpp

---
+++ Dynamic Programming/Minimizing Coins.cpp
@@ -0,0 +1,36 @@
+// Problem statement: https://cses.fi/problemset/task/1633
+// Solution
+
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) v.begin(), v.end()
+#define vi vector<int>
+#define ll long long
+ 
+const int N = 1000 * 1000;
+const int mod = 1e9 + 7;
+ 
+int dp[N + 9];
+vi coins;
+int n, x;
+int main() {
+	cin >> n >> x;
+	coins = vi(n);
+	for (int i = 0; i < n; ++i)
+		cin >> coins[i];
+ 
+	for (int i = 1; i <= N; ++i)
+		dp[i] = 1e7;
+ 
+	for (int i = 1; i <= x; ++i) {
+		for (int j = 0; j < n; ++j) {
+			if (i - coins[j] < 0)
+				continue;
+			dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+		}
+	}
+	if (dp[x] == 1e7)
+		cout << -1;
+	else
+		cout << dp[x];
+}
